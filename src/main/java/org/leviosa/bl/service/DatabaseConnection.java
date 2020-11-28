6923e4023bd25142186a32fea517cfa7173fc9ec
bhaduri <bhaduri.anirban@gmail.com>
2020-11-28 21:57:33 +0530

    Hedwig

--- src/main/java/org/leviosa/bl/service/DatabaseConnection.java
+++ src/main/java/org/leviosa/bl/service/DatabaseConnection.java
@@ -23,18 +23,18 @@ public class DatabaseConnection {
         DBPROPERTIES.put("javax.persistence.jdbc.user", DB_USER);
         DBPROPERTIES.put("javax.persistence.jdbc.password", DB_PASSWORD);
         
-        EMF = Persistence.createEntityManagerFactory("org.dgrf_DgrfCMSCore_jar_1.0-SNAPSHOTPU", DBPROPERTIES);
+        EMF = Persistence.createEntityManagerFactory("org.leviosa", DBPROPERTIES);
         
         //EMF = Persistence.createEntityManagerFactory("org.dgrf_FractalStudio_jar_1.0-SNAPSHOTPU");
     }
-    public DatabaseConnection(String DB_USER, String DB_PASSWORD, String DB_CONN_URL, String PU) {
-        HashMap<String, String> DBPROPERTIES = new HashMap<>();
-        DBPROPERTIES.put("javax.persistence.jdbc.url", DB_CONN_URL);
-        DBPROPERTIES.put("javax.persistence.jdbc.user", DB_USER);
-        DBPROPERTIES.put("javax.persistence.jdbc.password", DB_PASSWORD);
-        
-        EMF = Persistence.createEntityManagerFactory(PU, DBPROPERTIES);
-        
-        //EMF = Persistence.createEntityManagerFactory("org.dgrf_FractalStudio_jar_1.0-SNAPSHOTPU");
-    }    
+//    public DatabaseConnection(String DB_USER, String DB_PASSWORD, String DB_CONN_URL, String PU) {
+//        HashMap<String, String> DBPROPERTIES = new HashMap<>();
+//        DBPROPERTIES.put("javax.persistence.jdbc.url", DB_CONN_URL);
+//        DBPROPERTIES.put("javax.persistence.jdbc.user", DB_USER);
+//        DBPROPERTIES.put("javax.persistence.jdbc.password", DB_PASSWORD);
+//        
+//        EMF = Persistence.createEntityManagerFactory(PU, DBPROPERTIES);
+//        
+//        //EMF = Persistence.createEntityManagerFactory("org.dgrf_FractalStudio_jar_1.0-SNAPSHOTPU");
+//    }    
 }
